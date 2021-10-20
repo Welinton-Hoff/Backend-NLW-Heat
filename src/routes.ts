e8df62af0ca702b357ce016dcb7783da28d46944
Welinton-Hoff <62669413+Welinton-Hoff@users.noreply.github.com>
2021-10-19 23:22:20 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserSController } from "./controllers/ProfileUserSController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
+
+router.get("/message/last3", new GetLast3MessagesController().handle);
+
+router.get(
+  "/profile",
+  ensureAuthenticated,
+  new ProfileUserSController().handle);
+
+export { router };
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import { serverHttp } from "./app";
+
+serverHttp.listen(4000, () => console.log(`(º~°) Server is running on PORT 4000`))
